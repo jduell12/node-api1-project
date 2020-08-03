496a817c8b5c05041f1fe9494ca1c4e6fc2d6a4a
Jessica <duellje@oregonstate.edu>
2020-08-03 15:15:37 -0400

    put method works

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ server.get("/api/users/:id", (req, res) => {
   }
 });
 
-//creates user using information sent inside request body
+//creates user using information sent inside request body and returns the created user
 server.post("/api/users", (req, res) => {
   try {
     const userInfo = req.body;
@@ -62,12 +62,40 @@ server.post("/api/users", (req, res) => {
       res.status(201).json(userInfo);
     }
   } catch (err) {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
+  }
+});
+
+//updates the user with the specified id using data from the request body and returns the modified user
+server.put("/api/users/:id", (req, res) => {
+  try {
+    const editUser = req.body;
+    const { id } = req.params;
+    editUser.id = id;
+
+    let found = users.find((user) => user.id === id);
+
+    if (!found) {
+      res.status(404).json({
+        errorMessage: "The user with the specified ID does not exist.",
+      });
+    } else {
+      if (!editUser.name || !editUser.bio) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user" });
+      } else {
+        users = users.filter((user) => user.id !== id);
+        users.push(editUser);
+        res.status(200).json(editUser);
+      }
+    }
+  } catch (err) {
     res
       .status(500)
-      .json({
-        errorMessage:
-          "There was an error while saving the user to the database",
-      });
+      .json({ errorMessage: "The user information could not be modified." });
   }
 });
 
